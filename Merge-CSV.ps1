d967c2cbed69a40155059ebfd2f1785dad9c8e5e
mirrorgleam <mirrorgleam@users.noreply.github.com>
2019-02-22 15:33:07 -0500

    Update Merge-CSV.ps1

--- Merge-CSV.ps1
+++ Merge-CSV.ps1
@@ -20,7 +20,7 @@
     .OUTPUTS
         Will output a .csv
     .EXAMPLE  
-        Merge-CSV -CSVPath1 ".\file1.csv" -CSVPath2 ".\file2.csv" -CSVMergeKey1 "Addresses" -CSVMergeKey2 "IPs" -OutPath ".\Merged.csv"
+        Merge-CSV -CSVPath1 '.\file1.csv' -CSVPath2 '.\file2.csv' -CSVMergeKey1 'Addresses' -CSVMergeKey2 'IPs' -OutPath '.\Merged.csv'
 #>
 
 function Merge-CSV {
@@ -28,104 +28,143 @@ function Merge-CSV {
     [CmdletBinding()]
     param (
         # Path to first .csv file
-        [parameter(Mandatory=$True)]
+        [parameter( Mandatory = $True )]
         [string]$CSVPath1,
 
         # Path to second .csv file
-        [parameter(Mandatory=$True)]
+        [parameter( Mandatory = $True )]
         [string]$CSVPath2,
 
         # Column header to merge on in $CSVPath1
-        [parameter(Mandatory=$True)]
+        [parameter( Mandatory = $True )]
         [string]$CSVMergeKey1,
         
         # Column header to merge on in $CSVPath2
-        [parameter(Mandatory=$True)]
+        [parameter( Mandatory = $True )]
         [string]$CSVMergeKey2,
         
         # Path to output the merged .csv file
-        [parameter(Mandatory=$True)]
+        [parameter( Mandatory = $True )]
         [string]$OutPath
     )
 
     # Import .csv files to be merged
-    $CSVImport_1 = Import-Csv -Path $CSVPath1
-    $CSVImport_2 = Import-Csv -Path $CSVPath2
+    Try {
+        $CSVImport_1 = Import-Csv -Path $CSVPath1
+    } 
+    Catch {
+    $ImportError = @"
+"Unable to import $CSVPath1
+Check path and try again"
+"@
+        Write-Host $ImportError -ForegroundColor Red
+        Return
+    }
+
+    # Import .csv files to be merged
+    Try {
+        $CSVImport_2 = Import-Csv -Path $CSVPath2
+    } 
+    Catch {
+    $ImportError = @"
+"Unable to import $CSVPath2
+Check paths and try again"
+"@
+        Write-Host $ImportError -ForegroundColor Red
+        Return
+    }
+
+    # Test if $CSVMergeKey1 is in $CSVImport_1
+    if ($CSVMergeKey1 -notin $($CSVImport_1 | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name)) {
+        $KeyError = @"
+"The value you provided for CSVMergeKey1 `"$CSVMergeKey1`" 
+was not found in the header of $($CSVPath1.Split('\')[-1])
+Please check provided values and try again"
+"@
+        Write-host $KeyError -ForegroundColor Red
+        Return
+    }
 
     # Convert $CSVMergeKey2 to $CSVMergeKey1
-    if ($CSVMergeKey1 -ne $CSVMergeKey2) {
-        $CSVImport_2 = $CSVImport_2 |select @{N=$CSVMergeKey1; E={$_.$CSVMergeKey2}}, * -ExcludeProperty $CSVMergeKey2
+    if ($CSVMergeKey2 -notin $($CSVImport_2 | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name)) {
+        $KeyError = @"
+"The value you provided for CSVMergeKey2 `"$CSVMergeKey2`" 
+was not found in the header of $($CSVPath2.Split('\')[-1])
+Please check provided values and try again"
+"@
+        Write-host $KeyError -ForegroundColor Red
+        Return
     }
     
-    # Build list of CSV field headers
-    [System.Collections.ArrayList]$CSVFieldList_1 = $CSVImport_1 |`
-        gm -MemberType NoteProperty |select -ExpandProperty Name
-    [System.Collections.ArrayList]$CSVFieldList_2 = $CSVImport_2 |`
-        gm -MemberType NoteProperty |select -ExpandProperty Name 
+    Function Combine-Objects {
+        Param (
+            [Parameter(mandatory=$true)]$Object1, 
+            [Parameter(mandatory=$true)]$Object2
+        )
     
-    $CSVFieldList_2.Remove($CSVMergeKey1)
+        $arguments = [Pscustomobject]@()
+ 
+        foreach ( $Property in $Object1.psobject.Properties){
+            $arguments += @{$Property.Name = $Property.value}
+        
+        }
+ 
+        foreach ( $Property in $Object2.psobject.Properties){
+            $arguments += @{ $Property.Name= $Property.value}
+        
+        }
     
-    # Collect list of duplicate keys
-    [System.Collections.ArrayList]$CSVFieldListDupes = foreach($key in $CSVFieldList_1) {
-        if($key -in $CSVFieldList_2) { 
-            $key 
+        $Object3 = [Pscustomobject]$arguments
+    
+        return $Object3
+    }
+
+    $CSVFieldList_1 = $CSVImport_1[0].psobject.Properties.name
+    
+    if ($CSVMergeKey2 -in $CSVFieldList_1) {$CSVMergeKey2 = $CSVMergeKey2 + "2"}
+    
+    $CSVFieldList_2 = $CSVImport_2[0].psobject.Properties.name
+    
+    foreach ( $Key in $CSVFieldList_1 ) {
+        if ( $Key -in $CSVFieldList_2 ) { 
+            $CSVImport_2 = $CSVImport_2 | Select-Object @{ N=$Key+"2" ; E={ $_.$Key } } , * -ExcludeProperty $Key
+            $CSVFieldList_2[$CSVFieldList_2.IndexOf($Key)] = $Key + "2"
         } 
     }
+
+    $CSVFieldList_ALL = $CSVFieldList_1 + $CSVFieldList_2
+
+    $UniqueKeyValues = ($CSVImport_1.$csvMergeKey1 + $CSVImport_2.$csvMergeKey2) |sort |Get-Unique
+
+    $objectList = @()
+
+    foreach ($value in $UniqueKeyValues) {
+
+        $customObject = New-Object psobject
+        foreach ($item in $CSVFieldList_ALL) {Add-Member -InputObject $customObject -MemberType NoteProperty -Name $item -Value ""}
+
+        $CSVEntry_1 = $CSVImport_1 |where {$_.$csvMergeKey1 -eq $value}
+        $CSVEntry_2 = $CSVImport_2 |where {$_.$csvMergeKey2 -eq $value}
+
+        if ($CSVEntry_1 -and $CSVEntry_2) {
+            $combinedObjects = Combine-Objects -Object1 $CSVEntry_1 -Object2 $CSVEntry_2
+            foreach ($field in $CSVFieldList_ALL) {
+                $customObject.$field = $combinedObjects.$field
+            }
+         $objectList += $customObject
+        }
     
-    #If duplicate fields exist that are not the Merge Key modify duplicates in 2nd csv
-    if ($CSVFieldListDupes.Count -gt 0) {
-        foreach($dupe in $CSVFieldListDupes) {
-            $CSVImport_2 = $CSVImport_2 |select @{N=$dupe+"2"; E={$_.$dupe}}, * -ExcludeProperty $dupe
-            $CSVFieldList_2 = $CSVFieldList_2.replace($dupe,$dupe+"2")
+        elseif ($CSVEntry_1) {
+            foreach ($field in $CSVFieldList_ALL) {$customObject.$field = $CSVEntry_1.$field}
+            $objectList += $customObject
         }
+    
+        elseif ($CSVEntry_2) {
+            foreach ($field in $CSVFieldList_ALL) {$customObject.$field = $CSVEntry_2.$field}
+            $objectList += $customObject
+        } 
     }
 
-    # Collect unique merge values
-    [System.Collections.ArrayList]$uniqueKeyValues = $CSVImport_1.$CSVMergeKey1 + $CSVImport_2.$CSVMergeKey1 |sort -Unique
+    $objectList |Export-Csv -Path $OutPath -NoTypeInformation
 
-    # Collect unique header values
-    [System.Collections.ArrayList]$CSVFieldList_ALL = $CSVFieldList_1 + $CSVFieldList_2
-
-    #region Build hashtable with all unique Merge values and empty fields
-    [System.Collections.ArrayList]$AllHashList = @()
-
-    foreach ($value in $uniqueKeyValues) {
-        [System.Collections.Hashtable]$HashStore = @{}
-        foreach ($field in $CSVFieldList_ALL) {
-            $HashStore.Add("$field",'')
-        }
-        $HashStore.$CSVMergeKey1 = $value
-        $AllHashList += $HashStore
-    }
-
-    # Remove MergeField from FieldList 
-    $CSVFieldList_ALL.Remove($CSVMergeKey1)
-
-    # Update hashtable with data from imported csv files
-    foreach ($table in $AllHashList){
-        foreach ($value in $CSVFieldList_ALL) {
-            if ($table.$CSVMergeKey1 -in $CSVImport_1.$CSVMergeKey1 -and $value -in $CSVFieldList_1) {
-                $table.$value = $($CSVImport_1 |where {$_.$CSVMergeKey1 -eq $table.$CSVMergeKey1} |`
-                                select -ExpandProperty $value)
-            }
-            elseif ($table.$CSVMergeKey1 -in $CSVImport_2.$CSVMergeKey1 -and $value -in $CSVFieldList_2 -or $value -eq $CSVMergeKey1) {
-                $table.$value = $($CSVImport_2 |where {$_.$CSVMergeKey1 -eq $table.$CSVMergeKey1} |`
-                                select -ExpandProperty $value)
-            }
-            else {
-                $table.$value = ''
-            }
-        }
-    }
-
-    # Perform readability formatting on hashtables prior to exporting
-    foreach ($table in $AllHashList) {
-        foreach ($key in $($table.keys)) {
-            $table.$key = $table.$key |select -Unique
-            $table.$key = $table.$key -join "`r`n"
-        }
-    }
-
-    # Export final results
-    $AllHashList |ForEach-Object {[PSCustomObject]$_ }|Export-Csv -Path $OutPath -NoTypeInformation
 }
